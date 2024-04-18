37928bd6f804ac669d748ac83ded62c5e5011893
GoldGlaid <isavckov@yandex.ru>
2024-04-18 21:01:03 +0300

    Исправил pep 8

--- data_base/data_bd.py
+++ data_base/data_bd.py
@@ -1,23 +1,25 @@
 import sqlite3
 
 
-#db = sqlite3.connect('user.db')
-#Создание крусора
-#c = db.cursor()
-#закомитить таблицу чтобы при каждом запуске она еще раз не крафтилась
-#link_to_data -- ссылка на заметку
-#c.execute("""CREATE TABLE users (
+# db = sqlite3.connect('user.db')
+# Создание крусора
+# c = db.cursor()
+# закомитить таблицу чтобы при каждом запуске она еще раз не крафтилась
+# link_to_data -- ссылка на заметку
+# c.execute("""CREATE TABLE users (
 #   login text,
 #   password text,
 #  link_to_data text
-#)""")
-#db.commit()
-#db.close()
+# )""")
+# db.commit()
+# db.close()
 
 def close_table(db, c):
     db.commit()
     c.close()
     db.close()
+
+
 def serch_user(Login, Password):
     # Подключение к базе, создание курсора
     db = sqlite3.connect('users.db')
@@ -31,16 +33,17 @@ def serch_user(Login, Password):
     else:
         return False
 
+
 def registration(Login, Password):
-    #Подключение к базе, создание курсора
+    # Подключение к базе, создание курсора
     db = sqlite3.connect('user.db')
     c = db.cursor()
 
     c.execute("SELECT * FROM users WHERE Login={login}")
     value = c.fetchall()
-    #если таких значений не найдено value = [], приравнивается к false
+    # если таких значений не найдено value = [], приравнивается к false
     if value:
-        #Дописать ссылку на файлик
+        # Дописать ссылку на файлик
         c.execute("INSERT INTO users Login={login}, Password={password}, 'hcddhe.txt')")
     else:
         return "Такой пользователь уже есть!"
