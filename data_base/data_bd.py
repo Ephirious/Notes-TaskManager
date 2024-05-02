27732fc26f075ff9cd9974170503fcd5814a95ba
Alena10Github <sergeevanat71@icloud.com>
2024-05-02 14:45:57 +0300

    Исправила баги в базе данныч,добавила функцию добавления ссылки на папку пользователя

--- data_base/data_bd.py
+++ data_base/data_bd.py
@@ -20,6 +20,13 @@ def close_table(db, c):
     db.close()
 
 
+class User:
+    def __init__(self, login, password, data):
+        self.login = login
+        self.password = password
+        self.data = data
+
+
 def search_user(login, password):
     # Подключение к базе, создание курсора
     db = sqlite3.connect('data_base/user.db')
@@ -28,24 +35,44 @@ def search_user(login, password):
     c.execute(f"SELECT * FROM users WHERE login={login}")
     value = c.fetchall()
     close_table(db, c)
-    if value != [] and value[0][1] == password:
-        return True
+    if value and (value[0][0] == login and value[0][1] == password):
+        return User(*value[0])
     else:
         return False
 
 
-def registration(login, password):
+def loginDuplicateChecker(login):
+    db = sqlite3.connect('data_base/user.db')
+    c = db.cursor()
+
+    c.execute("SELECT * FROM users WHERE login=?", (login,))
+    value = c.fetchall()
+    if value:
+        close_table(db, c)
+        return True
+    else:
+        close_table(db, c)
+        return False
+
+
+def add_link_to_data(login, data):
+    db = sqlite3.connect('data_base/user.db')
+    c = db.cursor()
+
+    c.execute("UPDATE users  SET link_to_data=? WHERE login=?",
+              (data, login,))
+    close_table(db, c)
+
+
+def registration(login, password, data=None):
     # Подключение к базе, создание курсора
     db = sqlite3.connect('data_base/user.db')
     c = db.cursor()
 
-    c.execute("SELECT * FROM users WHERE login={login}")
+    c.execute("SELECT * FROM users WHERE login=?", (login,))
     value = c.fetchall()
     # если таких значений не найдено value = [], приравнивается к false
-    if value:
-        c.execute("INSERT INTO users  (login, password) VALUES ({}, {})".format(login, password))
-        close_table(db, c)
-        return True
-    else:
-        close_table(db, c)
-        return False
+    c.execute("INSERT INTO users  (login, password, link_to_data) VALUES (?, ?, ?)",
+              (login, password, data,))
+    close_table(db, c)
+    return User(login, password, data)
