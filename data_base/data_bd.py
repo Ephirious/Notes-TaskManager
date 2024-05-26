a5b3a5346f86cd12daca7477c5bc98dd52e3a0d2
Alena10Github <sergeevanat71@icloud.com>
2024-05-27 00:56:03 +0300

    Создала таблицу для хранения данных о задаче: номер задачи, название задача, время начала и конца, тег. Функции добавления и удаления задачи из базы данных

--- data_base/data_bd.py
+++ data_base/data_bd.py
@@ -1,19 +1,6 @@
 import sqlite3
 
 
-# db = sqlite3.connect('user.db')
-# Создание крусора
-# c = db.cursor()
-# закомитить таблицу чтобы при каждом запуске она еще раз не крафтилась
-# link_to_data -- ссылка на заметку
-# c.execute("""CREATE TABLE users (
-#   login text,
-#   password text,
-#  link_to_data text
-# )""")
-# db.commit()
-# db.close()
-
 def close_table(db, c):
     db.commit()
     c.close()
@@ -25,6 +12,31 @@ class User:
         self.login = login
         self.password = password
         self.data = data
+
+
+#def add_link_to_note(login, path):
+   # db = sqlite3.connect('data_base/user.db')
+   #  c = db.cursor()
+
+    # c.execute("INSERT INTO links  (login, path) VALUES (?, ?)",
+    #           (login, path))
+
+
+# def list_of_users_links(login):
+#     db = sqlite3.connect('data_base/user.db')
+#     c = db.cursor()
+
+    # c.execute("SELECT path FROM  links  WHERE login=?", (login,))
+    # list_of_links = c.fetchall()
+    # return list_of_links
+
+
+# def delete_link_to_data(path):
+#     db = sqlite3.connect('data_base/user.db')
+#     c = db.cursor()
+
+    # c.execute("DELETE FROM links WHERE path=?", (path,))
+
 
 
 def search_user(login, password):
@@ -76,6 +88,43 @@ def registration(login, password, data=None):
               (login, password, data,))
     close_table(db, c)
     return User(login, password, data)
+
+def add_task_to_bd(id, task_name, time_start, time_stop, teg):
+    db = sqlite3.connect('data_base/user.db')
+    c = db.cursor()
+    c.execute("INSERT INTO tasks  (id, task_name, time_start, time_stop, teg) VALUES (?, ?, ?, ?, ?)",
+              (id, task_name, time_start, time_stop, teg))
+    close_table(db, c)
+
+
+def delete_task_bd(task_id):
+    db = sqlite3.connect('data_base/user.db')
+    c = db.cursor()
+    c.execute("DELETE FROM tasks WHERE id=?", (task_id,))
+    close_table(db, c)
+
+
+db = sqlite3.connect('user.db')
+# Создание крусора
+c = db.cursor()
+# закомитить таблицу чтобы при каждом запуске она еще раз не крафтилась
+#link_to_data -- ссылка на заметку
+c.execute("""CREATE TABLE IF NOT EXISTS users (
+   login text,
+   password text,
+   link_to_data text, 
+   key text
+ )""")
+
+
+c.execute("""CREATE TABLE IF NOT EXISTS tasks (
+   id integer,
+   task_name text,
+   time_start text,
+   time_stop text,
+   teg text
+ )""")
+close_table(db, c)
 
 #Таблица для хранения данных о заметке
 #db = sqlite3.connect('user.db')
